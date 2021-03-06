773637fe9b2269de4d6715fffe895ec83c6069d7
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-03-06 14:15:33 +0100

    Use github token

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 use serde_json::Value;
-use clap::{Arg, App};
 use reqwest::header::USER_AGENT;
+use read_input::prelude::*;
+
+struct Auth {
+    client: reqwest::Client,
+    login: String,
+    token: String,
+}
+
+impl Auth {
+    fn new() -> Auth {
+        Auth{
+            client: reqwest::Client::new(),
+            login: String::new(),
+            token: String::new(),
+        }
+    }
+}
 
 #[tokio::main]
-async fn search(user: &String, client: &reqwest::Client) -> Result<Value, Box<dyn std::error::Error>> {
-    let url = format!("https://api.github.com/search/issues?q=author:{}+type:pr+is:open", user);
-    let payload = client.get(url.as_str())
+async fn search(auth: &Auth) -> Result<Value, Box<dyn std::error::Error>> {
+    let url = format!("https://api.github.com/search/issues?q=author:{}+type:pr+is:open", auth.login);
+    let payload = auth.client.get(url.as_str())
         .header(USER_AGENT, "GitHub Board")
+        .bearer_auth(auth.token.as_str())
         .send()
         .await?
         .json::<Value>()
@@ -15,27 +32,28 @@ async fn search(user: &String, client: &reqwest::Client) -> Result<Value, Box<dy
 }
 
 #[tokio::main]
-async fn get_pr_sha(pr: &Value, client: &reqwest::Client) -> Result<Value, Box<dyn std::error::Error>> {
+async fn get_pr_sha(pr: &Value, auth: &Auth) -> Result<Value, Box<dyn std::error::Error>> {
     let repository_url = pr["repository_url"].as_str().unwrap();
     let number = pr["number"].as_u64().unwrap();
     let url = format!("{}/pulls/{}", repository_url, number);
-    let payload = client.get(url.as_str())
+    let payload = auth.client.get(url.as_str())
         .header(USER_AGENT, "My Rust Program 1.0")
+        .bearer_auth(auth.token.as_str())
         .send()
         .await?
         .json::<Value>()
         .await?;
-    println!("{}", payload);
     Ok(payload)
 }
 
 #[tokio::main]
-async fn get_pr_status(pr: &Value, sha: &Value, client: &reqwest::Client) -> Result<Value, Box<dyn std::error::Error>> {
+async fn get_pr_status(pr: &Value, sha: &Value, auth: &Auth) -> Result<Value, Box<dyn std::error::Error>> {
     let repository_url = pr["repository_url"].as_str().unwrap();
     let commit = sha["head"]["sha"].as_str().unwrap();
     let url = format!("{}/commits/{}/status", repository_url, commit);
-    let payload = client.get(url.as_str())
+    let payload = auth.client.get(url.as_str())
         .header(USER_AGENT, "My Rust Program 1.0")
+        .bearer_auth(auth.token.as_str())
         .send()
         .await?
         .json::<Value>()
@@ -44,31 +62,21 @@ async fn get_pr_status(pr: &Value, sha: &Value, client: &reqwest::Client) -> Res
 }
 
 fn main() {
-    let matches = App::new("GitHub Board")
-            .arg(Arg::new("user")
-                .short('u')
-                .long("user")
-                .value_name("USER")
-                .about("Specify GitHub user login")
-                .required(true))
-        .get_matches();
-    let user = match matches.value_of("user") {
-        Some(k) => String::from(k),
-        None => panic!("User login not provided"),
-    };
-    let client = reqwest::Client::new();
-    let res = match search(&user, &client) {
+    let mut auth = Auth::new();
+    auth.login = input().msg("login: ").get();
+    auth.token = rpassword::prompt_password_stdout("token: ").unwrap();
+    let res = match search(&auth) {
         Ok(s) => s,
         Err(e) => panic!("Fail to search: {}", e),
     };
     let prs = res["items"].as_array().unwrap();
     for pr in prs {
-        let sha = match get_pr_sha(&pr, &client){
+        let sha = match get_pr_sha(&pr, &auth){
             Ok(s) => s,
             Err(e) => panic!("Fail to fetch sha: {}", e),
         };
-        match get_pr_status(&pr, &sha, &client){
-            Ok(s) => println!("{} {}", pr["repository_url"], s["state"]),
+        match get_pr_status(&pr, &sha, &auth){
+            Ok(s) => println!("{} {}", pr["url"], s["state"]),
             Err(e) => panic!("Fail to fetch sha: {}", e),
         };
     }
